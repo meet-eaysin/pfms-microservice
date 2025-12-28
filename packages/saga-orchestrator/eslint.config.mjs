847ce467afc581c80fdee6021f5c44d66e632201
eaysin <eaysin.dev@gmail.com>
2025-12-28 20:54:57 +0600

    feat: remove saga orchestrator package

--- packages/saga-orchestrator/eslint.config.mjs
+++
@@ -1,9 +0,0 @@
-import { config as baseConfig } from '@pfms/eslint-config/base';
-
-export default [
-  ...baseConfig,
-  {
-    files: ['**/*.ts'],
-    ignores: ['dist/**', 'node_modules/**', '**/*.spec.ts'],
-  },
-];
--- packages/saga-orchestrator/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { SagaCoordinator } from './saga-coordinator';
-export { SagaStateManager } from './saga-state-manager';
-export { CompensationHandler } from './compensation-handler';
-export { TimeoutManager } from './timeout-manager';
-export * from './types';
