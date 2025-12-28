c0ead4c9955c0b094ce0780e99ae83b8a16bf104
meet-eaysin <meet.eaysin@gmail.com>
2025-12-28 10:05:46 +0600

    feat: Add Dockerfiles, Jest configurations, and environment files for expense, income, ledger, and planning services

---
+++ apps/expense-service/jest.config.e2e.js
@@ -0,0 +1,34 @@
+module.exports = {
+  displayName: 'expense-service-e2e',
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  rootDir: '.',
+  testMatch: ['<rootDir>/test/e2e/**/*.spec.ts'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/*.interface.ts',
+    '!src/**/*.module.ts',
+    '!src/main.ts',
+  ],
+  coverageDirectory: 'coverage/e2e',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  testTimeout: 60000,
+  verbose: true,
+  forceExit: true,
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true,
+  setupFilesAfterEnv: ['<rootDir>/test/setup-e2e.ts'],
+};
---
+++ apps/income-service/jest.integration.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  displayName: 'income-service-integration',
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  rootDir: '.',
+  testMatch: ['<rootDir>/test/integration/**/*.spec.ts'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/*.interface.ts',
+    '!src/**/*.module.ts',
+    '!src/main.ts',
+  ],
+  coverageDirectory: 'coverage/integration',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  testTimeout: 30000,
+  verbose: true,
+  forceExit: true,
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true,
+};
